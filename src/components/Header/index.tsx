58bb2f2d2771372d73daaec7d79a6a187e44f250
xuyangyang3 <xuyangyang3@xiaomi.com>
2024-08-02 21:50:43 +0800

    feat: header组件完成

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,27 +1,61 @@
-import { forwardRef, useState } from 'react';
-import { MenuFoldOutlined, MenuUnfoldOutlined } from '@ant-design/icons';
-import { Button, Layout, theme } from 'antd';
+import { MenuUnfoldOutlined } from '@ant-design/icons';
+import { Button, Layout, Avatar, Dropdown } from 'antd';
+import type { MenuProps } from 'antd';
+import './index.css';
+import avatar from '@/assets/images/avatar.jpg';
 
-const View = forwardRef(() => {
-    const [collapsed, setCollapsed] = useState<boolean>(false);
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
+const View = () => {
+    const logOut = () => {
+        console.log('退出登录');
+    };
+    const items: MenuProps['items'] = [
+        {
+            key: '1',
+            label: (
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.antgroup.com"
+                >
+                    个人中心
+                </a>
+            ),
+        },
+        {
+            key: '2',
+            label: (
+                <a
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    href="https://www.aliyun.com"
+                    onClick={() => {
+                        logOut();
+                    }}
+                >
+                    退出登录
+                </a>
+            ),
+        },
+    ];
 
     return (
-        <Layout.Header style={{ padding: 0, background: colorBgContainer }}>
+        <Layout.Header className="header-container">
             <Button
                 type="text"
-                icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
-                onClick={() => setCollapsed(!collapsed)}
+                icon={<MenuUnfoldOutlined />}
+                // onClick={() => setCollapsed(!collapsed)}
                 style={{
                     fontSize: '16px',
                     width: 64,
-                    height: 64,
+                    height: 32,
+                    backgroundColor: '#fff',
                 }}
             />
+            <Dropdown menu={{ items }}>
+                <Avatar size={32} src={<img src={avatar}></img>}></Avatar>
+            </Dropdown>
         </Layout.Header>
     );
-});
+};
 
 export default View;
